a4d825b9e3ace89ab2d54a2f4d986b3e284b5568
laracostalcsc <laracosta.lcsc@gmail.com>
2025-02-16 19:20:23 +0000

    Manual-e-Regras-Super-Trunfo-Geografico

---
+++ Manual-e-Regras-Super-Trunfo-Geografico.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    printf("Super Trunfo Geográfico - Regras de Jogo:\n");
+    printf("\n");
+
+    printf("Participantes: 2 ou mais.\n");
+
+    printf("Idade: recomendado a partir de 7 anos.\n");
+    printf("\n");
+
+    printf("Cadastro das Cartas:\n");
+    printf("\n");
+    printf("São 32 cartas.\n");
+    printf("\n");
+
+    printf("Objetivo:\n");
+    printf("Ficar com todas as cartas do baralho.\n");
+    printf("\n");
+
+    printf("Preparação:\n");
+    printf("As cartas são distribuídas em número igual para cada um dos jogadores. Cada jogador forma seu monte e só vê a primeira carta da pilha. As cartas possuem informações sobre os países como: área, população, densidade demográfica, PIB e IDH. É com estas informações que cada um vai jogar. \n");
+    printf("Como jogar:\n");
+    printf("\n");
+
+    printf("Se você é o primeiro a jogar,escolha, entre as informações contidas em sua primeira carta, aquela que você julga ter o valor capaz de superar o valor da mesma informação que se encontra na carta que seus adversários têm em mãos. Por exemplo: você escolhe a informação população, menciona-a em voz alta e abaixa a carta na mesa. Imediatamente todos os outros jogadores abaixam a primeira carta de suas pilhas e conferem o valor da informação. Quem tiver o valor mais alto ganha as cartas da mesa e as coloca embaixo de sua pilha. O próximo jogador será o que venceu a rodada anterior. Assim prossegue o jogo até que um dos participantes fique com todas as cartas baralho, vencendo a partida. Se dois ou mais jogadores abaixam cartas com o mesmo valor máximo, os demais participantes deixam suas cartas na mesa e a vitória é decidida entre os que empataram. Para isso, quem escolheu inicialmente diz um novo item de sua próxima carta, ganhando as cartas da rodada quem tiver o valor mais alto.\n");
+    printf("\n");
+
+    printf("Bom aprendizado e divertimento!\n");
+
+
+    return 0;
+
+}
